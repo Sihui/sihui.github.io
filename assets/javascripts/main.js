be4f4075ef1f5191e0469f53086ed986d8ee740a
Sihui Huang <sihuih@gmail.com>
2014-09-05 01:25:34 -0500

    add vertical centering and init Greetings

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -5,3 +5,20 @@ $(window).load(function() {
     $("#overlay").delay(1000).fadeOut();
     $("section").delay(2000).css('opacity', '1');
 });
+
+/* Vertical Centering
+*********************************************************************************************/
+
+$('.centered').each(function(){
+    $container = $(this);
+    $section = $container.parent();
+    var margin_top = ($section.height() - $container.height())/2;
+    $container.css('margin-top',margin_top);
+});
+
+/* Top Bounce Animation
+*********************************************************************************************/
+setInterval(function() {
+  $('.greetings').toggleClass("animated bounce");
+  //console.log('wow!');
+}, 2000);
